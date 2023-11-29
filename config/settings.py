b719344743574ed8e9fe67d8c57bd303d8a6f9b9
Kevin Andres Rosales Marquez <kevinarm@Kevins-Laptop.local>
2023-11-28 23:44:18 -0700

    DJANGO_DEBUG

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DJANGO_DEBUG')
 if DEBUG is False:
     SECURE_SSL_REDIRECT = True
 ALLOWED_HOSTS = [
