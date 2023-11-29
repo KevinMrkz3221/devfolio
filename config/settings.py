d8d7ecc824456c35e6cfc93b273b1d23fae3c8e5
Kevin Andres Rosales Marquez <kevinarm@Kevins-Laptop.local>
2023-11-29 00:58:18 -0700

    More settings

--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,17 @@ if DEBUG is False:
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
+
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    X_FRAME_OPTIONS = 'DENY'
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_REFERRER_POLICY = 'strict-origin'
+
+
     
+
 ALLOWED_HOSTS = [
     'kevinarm.me',
     'www.kevinarm.me',
