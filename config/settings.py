a2269d60a873a4c41dacecb23f81b59286f48069
Kevin Andres Rosales Marquez <kevinarm@Kevins-Laptop.local>
2023-11-28 21:29:30 -0700

    Secret

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(2_!3eeow9g8vz_*k8obqcev658#thylk&cbryh+8*-s!u%0#r'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
