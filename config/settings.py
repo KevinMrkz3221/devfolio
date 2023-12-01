83a097e563dc1c1d3392153a05520746a8cd3a99
Kevin Andres Rosales Marquez <kevinarm@Kevins-Laptop.local>
2023-12-01 13:21:39 -0700

    Llenando

--- config/settings.py
+++ config/settings.py
@@ -110,7 +110,19 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'db_antrosapp',
+#         'USER': 'user_antros',
+#         'PASSWORD': 'iKxRihNChVsvqUnG2wNX',
+#         'HOST': 'db-datalori-prod.cu4mrovcngvy.us-east-2.rds.amazonaws.com',    # Puedes cambiarlo si tu base de datos está en un servidor remoto
+#         'PORT': '3306',         # El puerto predeterminado de MySQL
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
