4d3bb96cadda40f8359261a6cd62b0dc0b684e9c
Kevin Andres Rosales Marquez <kevinarm@Kevins-Laptop.local>
2023-12-15 10:30:40 -0700

    SSL

--- config/settings.py
+++ config/settings.py
@@ -28,8 +28,9 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG')
 if DEBUG is False:
-    SECURE_SSL_REDIRECT = True
+    
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_HSTS_SECONDS = 31536000
